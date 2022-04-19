cbf3ad66f06da242faa117fc1a8f8716bf26a6ad
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-04-18 22:09:56 -0500

    showing the current Temp, humidity and wind speed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#currentTemp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = `913b7ac1ecf2018545f41afe76c8aad3`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=${apiKey}&units=imperial`;
 
+axios.get(apiUrl).then(displayTemperature);
