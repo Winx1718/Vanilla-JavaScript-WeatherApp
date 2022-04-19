5c9da3378bdd271c037007e0c0c42101af5541c6
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-04-18 22:41:43 -0500

    created time and date functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let day = date.getDay();
+  let dayOfMonth = date.getDate();
+  let month = date.getMonth() + 1;
+  return `${day} ${month}/${dayOfMonth}`;
+}
+function formatTime(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#currentTemp");
@@ -5,12 +19,17 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let timeElement = document.querySelector("#time");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
 }
+
 let apiKey = `913b7ac1ecf2018545f41afe76c8aad3`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=${apiKey}&units=imperial`;
 
