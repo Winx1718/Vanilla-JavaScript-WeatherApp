7aeff5bc06a60baa3b1e5dc62fbd71e9fa763e04
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-05-02 21:57:09 -0500

    added forecast description and format for forecast Icon

--- src/index.js
+++ src/index.js
@@ -58,7 +58,9 @@ function displayForecast(response) {
                 <h5 class="forecastDays">${formatDay(forecastDay.dt)}</h5>
                 <img src="http://openweathermap.org/img/wn/${
                   forecastDay.weather[0].icon
-                }@2x.png" alt="" />
+                }@2x.png" alt="${
+          forecastDay.weather[0].description
+        }" class=forecastImg />
                 <p class="forecastTemp">${Math.round(
                   forecastDay.temp.max
                 )}° / ${Math.round(forecastDay.temp.min)}°</p>
@@ -73,10 +75,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = `913b7ac1ecf2018545f41afe76c8aad3`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
